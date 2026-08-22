fbb566c2d7810de5d606320d6c20fbded125f691
Your Name <you@example.com>
2026-08-22 17:13:40 +0200

    simple login frm the backend

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "accounts",
     "api",
     "corsheaders",
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,12 @@ MAILERS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",    
 ]
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
