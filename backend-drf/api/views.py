9865d2af82828c36287e455cc9abc623fbbdd5b0
Your Name <you@example.com>
2026-08-26 16:35:14 +0200

    all the important fonction are done and i integret ml fonction with django & react app now there are few think can be changed in the future

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import os
 from django.conf import settings
+from .utils import save_plot
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Create your views here.
 class StockPredictionAPIView(APIView):
@@ -38,16 +42,103 @@ class StockPredictionAPIView(APIView):
                     
             # save the plot to a file
             plot_img_path = f'{ticker}_plot.png'
-            image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
+            plot_img = save_plot(plot_img_path)
 
-            plt.savefig(image_path)
-            plt.close()
+            # 100 days moving average
+            ma100 = df.Close.rolling(100).mean()
+            plt.switch_backend('AGG')
+            plt.figure(figsize =(12,5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.plot(ma100,'r',label='100 DMA')
+            plt.title(f"100 days  moving average of{ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Close Price')
+            plt.legend()           
+            plot_img_path = f'{ticker}_100_dma.png'
+            plot_100_dma = save_plot(plot_img_path)
+            
+            # 200 days moving average
+            
+            ma200 = df.Close.rolling(200).mean()
+            plt.switch_backend('AGG')
+            plt.figure(figsize =(12,5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.plot(ma100,'r',label='100 DMA')
+            plt.plot(ma200,'g',label='200 DMA')
+            plt.title(f"200 days moveing average of{ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Close Price')
+            plt.legend()           
+            plot_img_path = f'{ticker}_200_dma.png'
+            plot_200_dma = save_plot(plot_img_path)    
+            
+            
+            
+            # spliting data into training and testing data sets
+            data_training = pd.DataFrame(df.Close[0:int(len(df)*0.7)])
+            data_testing = pd.DataFrame( df.Close[int(len(df)*0.7):int(len(df))])   
+            
+            # scaling down the data between 0 and 1 
+            scaler = MinMaxScaler(feature_range=(0,1))
+            
+            # load ml model
+            model = load_model('stock_prediction_model.keras')
+            
+            # preparing the test data
+            past_100_days = data_training.tail(100)
+            final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+            input_data = scaler.fit_transform(final_df)
+            
+            
+            
+            x_test = []
+            y_test = []
 
-            plot_img = settings.MEDIA_URL + plot_img_path
+            for i in range(100, input_data.shape[0]):
+                    x_test.append(input_data[i-100: i])
+                    y_test.append(input_data[i,0])
+            x_test, y_test = np.array(x_test), np.array(y_test)
 
-            print(plot_img)
+        
+        # making prediction
+            y_predicted = model.predict(x_test)
+            
+            
+        # revert the scales prices to original price
+            y_predicted =scaler.inverse_transform(y_predicted.reshape(-1,1)).flatten()
+            y_test = scaler.inverse_transform(y_test.reshape(-1,1)).flatten()
 
+        # plot the final prediction
+            plt.switch_backend('AGG')
+            plt.figure(figsize =(12,5))
+            plt.plot(y_test,'b', label='Original Price')
+            plt.plot(y_predicted,'r',label='Predicted Price')
+            plt.title(f"Final Prediction for{ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Close Price')
+            plt.legend()           
+            plot_img_path = f'{ticker}_final_prediction.png'
+            plot_prediction = save_plot(plot_img_path)                       
+                
+            # Model evaluation
+            
+            # Mean squad error
+            mse = mean_squared_error(y_test, y_predicted)
+            
+            # Root mean squad error
+            rmse = np.sqrt(mse)
+            
+            # R-Squared
+            r2 = r2_score(y_test,y_predicted)
+           
             return Response({
                 'status': 'success',
-                'plot_img': plot_img
+                'plot_img': plot_img,
+                'ploot_100_dma': plot_100_dma,
+                'plot_200_dma': plot_200_dma,
+                'plot_prediction': plot_prediction,
+                'mse': mse,
+                'rmse': rmse,
+                'r2': r2,
+                
             })
--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -8,6 +8,14 @@ const Dashboard = () => {
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const [plot, setPlot] = useState()
+    const [ma100, setMA100] = useState()
+    const [ma200, setMA200] = useState()
+    const [prediction, setPrediction] = useState()
+    const [mse,setMSE] = useState()
+    const [rmse,setRMSE] = useState()
+    const [r2,setR2] = useState()
+
+
 
     useEffect(() => {
         const fetchProtectedData = async () => {
@@ -34,13 +42,24 @@ const Dashboard = () => {
             });
             console.log(response.data);
             const backendRoot = import.meta.env.VITE_BACKEND_ROOT;
-            const plotURL = `${backendRoot}${response.data.plot_img}`;
+            const plotURL = `${backendRoot}${response.data.plot_img}`
+            const ma100URL = `${backendRoot}${response.data.plot_100_dma}`
+            const ma200URL = `${backendRoot}${response.data.plot_200_dma}`
+            const predictionURL = `${backendRoot}${response.data.plot_200_dma}`
+            
+
 
             console.log("Backend root:", backendRoot);
             console.log("Backend response:", response.data);
             console.log("Final image URL:", plotURL);
 
-setPlot(plotURL);
+            setPlot(plotURL);
+            setMA100(ma100URL);
+            setMA200(ma200URL);
+            setPrediction(predictionURL);
+            setMSE(response.data.mse);
+            setRMSE(response.data.rmse);
+            setR2(response.data.r2);
             // set plots
 
             if(response.data.error){
@@ -79,16 +98,42 @@ setPlot(plotURL);
             </form>
         </div>
         {/* print predction plots       */ }
-        <div className="prediction mt-5">
+        {prediction && (
+            <div className="prediction mt-5">
             <div className="p-5 text-center">
               {plot && (
                         <img src={plot} style={{ maxWidth: '100%' }} />
                     )}
+                    </div>
+                    <div className="pt-3">
+                        {ma100 &&(
+                            <img src={ma100} style={{ maxWidth: '100%' }} />
+                        )}
 
+                    </div>
+                    <div className="pt-3">
+                        {ma200 &&(
+                            <img src={ma200} style={{ maxWidth: '100%' }} />
+                        )}
 
-            </div>
+                    </div>
+                     <div className="pt-3">
+                        {prediction &&(
+                            <img src={prediction} style={{ maxWidth: '100%' }} />
+                        )}
+
+                    </div>
+                    <div className="text-light p-3">
+                            <h4>Model Evaluation</h4>
+                            <p>Mean squared Error (MSE):{ mse}</p>
+                            <p> Root Mean squared Error (RMSE):{ rmse}</p>
+                            <p>R-Squared (R2):{ r2}</p>
+                    </div>
+                    
 
         </div>
+        )}
+        
 
     </div>
 </div>
